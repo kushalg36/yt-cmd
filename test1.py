869f5d5c42c5a3e89361afee62a090132a692a31
kushalg36 <kushalgupta_de@srmuniv.edu.in>
2020-03-11 23:45:38 +0530

    test objects

---
+++ test1.py
@@ -0,0 +1,36 @@
+from apiclient.discovery import build
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+api_key = 'AIzaSyAPdA0K_BWOMddIl3iHUl39UNeI4qXOsJk'
+
+youtube = build('youtube','v3',developerKey=api_key)
+
+request = youtube.search().list(
+    part = "snippet",
+    maxResults = '1',
+    q = 'homicide logic'
+)
+
+response = request.execute()
+
+video_id = response['items'][0]['id']['videoId']
+
+# print(response['contentDetails']['duration'])
+
+request = youtube.videos().list(
+        part="snippet,contentDetails,statistics",
+        id=video_id
+    )
+
+response = request.execute()
+
+# print(response['contentDetails']['duration'])
+
+video_duration =  response['items'][0]['contentDetails']['duration']
+
+print(video_duration)
+
+duration_ms = (int(video_duration[ video_duration.find("M")-1 ]) * 60000 ) + (int(video_duration[video_duration.find("M")+1]) * 10000 ) + (int(video_duration[video_duration.find("S")-1]) * 1000 )
+
+print(duration_ms)
