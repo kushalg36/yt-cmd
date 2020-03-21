dba84ca5a8ceb98ad0c3c3f44bbe055f544c7472
kushalg36 <kushalgupta_de@srmuniv.edu.in>
2020-03-21 12:21:18 +0530

    updated gitignore

---
+++ run_noclass.py
@@ -0,0 +1,74 @@
+from logic import logic
+import keyboard 
+import sys,traceback
+import argparse
+import time
+
+'''
+ TODO
+ 1) handle keyboardInterrupt ---DONE
+ 2) handle MaxRetryError ---DONE
+ 3) add graphics
+ 4) add colors
+ 5) show video name and artist ---DONE
+ 6) ctrl + C should also stop music
+ 7) wait until
+'''
+
+# call logic here
+
+
+def delete_lines(n):
+    for i in range(n):
+        sys.stdout.write(SHIFT_UP)
+        sys.stdout.write(DELETE_LINE)
+
+
+def args_parser():
+    """
+    Parse Command line arguments
+    """
+    parser = argparse.ArgumentParser(description='yt-play: Play music from terminal')
+    parser.add_argument('-s','--song',type=str,help='Enter song name to be played')
+    return parser.parse_args()
+
+def format(args):
+    """
+    Key formating
+    """
+    try: 
+        yt_app = logic()
+        yt_app.search(args.song)
+
+        while keyboard.is_pressed('q') == False:
+            if(keyboard.is_pressed('s') == True):
+                song = input('Enter new song==> ')
+                yt_app.search(query = song) #Not working yet
+                time.sleep(0.1)
+
+            elif(keyboard.is_pressed('l') == True):
+                yt_app.next() #Working without issues
+                time.sleep(0.1)
+
+            elif(keyboard.is_pressed('f') == True):
+                yt_app.play_pause() #Working without issues
+                time.sleep(0.1)
+
+            elif(keyboard.is_pressed('g') == True):
+                yt_app.seek_forward() #Not working proerply (seeking 10 seconds more)
+                time.sleep(0.1) #working with no keypress in 5 milisecond
+            
+            elif(keyboard.is_pressed('d') == True):
+                count = yt_app.seek_backward() #Not working proerply (seeking 10 seconds more)
+                time.sleep(0.1) #working with no keypress in 5 milisecond
+
+            elif(keyboard.is_pressed('q') == True):
+                yt_app.close() # working but need to quit terminal command
+
+if __name__ == "__main__":
+    args = args_parser()
+    format(args)
+    except KeyboardInterrupt:
+        print('Shutdown requested...exiting')
+    except Exception:
+        print('Something went wrong!')
